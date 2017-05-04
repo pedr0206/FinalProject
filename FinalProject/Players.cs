de11e956b55e819492149a1d050079d58a35b02e
pedr0206 <pedr0209@edu.eal.dk>
2017-05-04 15:05:56 +0200

    Final Project

---
+++ FinalProject/Players.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalProject
+{
+    class Players
+    {
+        public int firstName { get; set; }
+        public int LastName { get; set; }
+        public int DateOfBirth { get; set; }
+        public int Team { get; set; }
+
+        public Players()
+        {
+
+        }
+    }
+}
